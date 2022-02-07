ccad4669ea57ca4a1396da5242bff111c3856dee
Eunho Lee <eunho.ambition@gmail.com>
2022-02-08 03:55:58 +0900

    feat: add condition for finish_poll (#4)

--- src/contract.rs
+++ src/contract.rs
@@ -59,7 +59,7 @@ pub fn execute(
     match msg {
         ExecuteMsg::Bet { side } => try_bet(deps, _env, info, side),
         ExecuteMsg::CancelBet { side } => try_cancel_bet(deps, _env, info, side),
-        ExecuteMsg::FinishPoll { winner } => try_finish_poll(deps, info, winner),
+        ExecuteMsg::FinishPoll { winner } => try_finish_poll(deps, _env, info, winner),
         ExecuteMsg::RevertPoll {} => try_revert_poll(deps, info),
         ExecuteMsg::Claim {} => try_claim(deps, info),
     }
@@ -235,7 +235,12 @@ pub fn try_cancel_bet(deps: DepsMut, env: Env, info: MessageInfo, side: u8) -> S
         })))
 }
 
-pub fn try_finish_poll(deps: DepsMut, info: MessageInfo, winner: u8) -> StdResult<Response> {
+pub fn try_finish_poll(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    winner: u8,
+) -> StdResult<Response> {
     let mut state = read_state(deps.storage)?;
 
     // only contract's owner can finish poll
@@ -250,9 +255,12 @@ pub fn try_finish_poll(deps: DepsMut, info: MessageInfo, winner: u8) -> StdResul
         return Err(StdError::generic_err("already finished poll"));
     }
 
-    // TODO: end_time check or not?
-    // if end time check => safe but not flexible
-    // else => more flexible policy but more responsiblity to owner *current implementation
+    // cannot finish before poll ends
+    if env.block.time < Timestamp::from_seconds(state.bet_end_time) {
+        return Err(StdError::generic_err(
+            "bet is live now, The poll cannot be finished before the bet ends",
+        ));
+    }
 
     let winner_amount = match SIDE_TOTAL_AMOUNT.may_load(deps.storage, &winner.to_be_bytes())? {
         Some(value) => value,
