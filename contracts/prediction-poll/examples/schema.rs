b2aaae7f8c63af4540c6b8f20371c83f79f18f69
TAEKEON GO <taekeongo@gmail.com>
2022-02-26 13:29:40 +0900

    Rearrange directory

--- contracts/prediction-poll/examples/schema.rs
+++ contracts/prediction-poll/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use pollterra_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use pollterra_contract::state::State;
+use prediction_poll::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use prediction_poll::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
