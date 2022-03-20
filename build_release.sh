2e8cc2b3b1b11e527b530f7d83034f3ead923866
TAEKEON GO <taekeongo@gmail.com>
2022-03-20 17:29:00 +0900

    add a trouble shooting comment to the build script

--- build_release.sh
+++ build_release.sh
@@ -3,3 +3,7 @@ docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   greengaguri/workspace-optimizer:0.12.5-protoc
+
+# If you encounter the error message related to protoc,
+# it can be caused by the old volumes made by cosmwasm/workspace-optimizer:0.12.5,
+# so retry this script after cleaning up the volumes;pollterra-token_cache, registry_cache
