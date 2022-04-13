eaf32f1f3c3815ad378c27037a9c17c851d9d713
eunho-lee <eunho.ambition@gmail.com>
2022-04-13 21:39:57 +0900

    fix: update_admins_failed_unauthorized

--- contracts/distributor/src/tests.rs
+++ contracts/distributor/src/tests.rs
@@ -6,6 +6,7 @@ mod community_tests {
     use testutils::mock_querier::mock_dependencies;
 
     use crate::entrypoints;
+    use crate::error::ContractError;
     use crate::msg::{ExecuteMsg, InstantiateMsg};
     use crate::query_msgs::{ContractConfigResponse, DistributionsResponse, QueryMsg};
 
@@ -13,6 +14,7 @@ mod community_tests {
     const CREATOR: &str = "creator";
     const ADMIN_0: &str = "admin0";
     const ADMIN_1: &str = "admin1";
+    const NOT_ADMIN: &str = "not_admin";
     const NEW_ADMIN: &str = "new_admin";
     const RECIPIENT: &str = "recipient";
 
@@ -66,19 +68,14 @@ mod community_tests {
         let mut deps = mock_dependencies(&[]);
         mock_instantiate(deps.as_mut());
 
-        let info = mock_info(ADMIN_0, &[]);
+        let info = mock_info(NOT_ADMIN, &[]);
         let admins = Some(vec![ADMIN_0.to_string(), NEW_ADMIN.to_string()]);
         let msg = ExecuteMsg::UpdateAdmins { admins };
-        let _res = entrypoints::execute(deps.as_mut(), mock_env(), info, msg);
 
-        let msg = QueryMsg::Config {};
-        let res: ContractConfigResponse =
-            from_binary(&entrypoints::query(deps.as_ref(), mock_env(), msg).unwrap()).unwrap();
-
-        assert_eq!(POLLTERRA_TOKEN, &res.managing_token);
-        assert_eq!(ADMIN_0, res.admins.get(0).unwrap());
-        assert_ne!(ADMIN_1, res.admins.get(1).unwrap());
-        assert_eq!(NEW_ADMIN, res.admins.get(1).unwrap());
+        assert!(matches!(
+            entrypoints::execute(deps.as_mut(), mock_env(), info, msg),
+            Err(ContractError::Unauthorized {})
+        ));
     }
 
     #[test]
