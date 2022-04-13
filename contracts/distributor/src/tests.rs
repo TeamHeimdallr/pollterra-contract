230faab9ecf7e9dbcb19947b8fa9cc702cd33461
eunho-lee <eunho.ambition@gmail.com>
2022-04-13 22:34:53 +0900

    add: distributor test code for distribute and transfer

--- contracts/distributor/src/tests.rs
+++ contracts/distributor/src/tests.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
-mod community_tests {
+mod distributor_tests {
     use cosmwasm_std::testing::{mock_env, mock_info, MOCK_CONTRACT_ADDR};
-    use cosmwasm_std::{from_binary, DepsMut, Uint128};
+    use cosmwasm_std::{
+        attr, from_binary, to_binary, CosmosMsg, DepsMut, SubMsg, Uint128, WasmMsg,
+    };
+    use cw20::Cw20ExecuteMsg;
 
     use testutils::mock_querier::mock_dependencies;
 
@@ -17,6 +20,7 @@ mod community_tests {
     const NOT_ADMIN: &str = "not_admin";
     const NEW_ADMIN: &str = "new_admin";
     const RECIPIENT: &str = "recipient";
+    const RECIPIENT_2: &str = "recipient2";
 
     fn mock_instantiate(deps: DepsMut) {
         let msg = InstantiateMsg {
@@ -256,6 +260,164 @@ mod community_tests {
             end_height: Some(new_end_height),
             amount: Some(new_amount),
             message: None,
+        };
+
+        let info = mock_info(NOT_ADMIN, &[]);
+        assert!(matches!(
+            entrypoints::execute(deps.as_mut(), mock_env(), info, msg),
+            Err(ContractError::Unauthorized {})
+        ));
+    }
+
+    #[test]
+    fn proper_distribute() {
+        let mut deps = mock_dependencies(&[]);
+        mock_instantiate(deps.as_mut());
+
+        deps.querier.with_token_balances(&[(
+            &POLLTERRA_TOKEN.to_string(),
+            &[(&MOCK_CONTRACT_ADDR.to_string(), &Uint128::from(20_000u128))],
+        )]);
+
+        let info = mock_info(ADMIN_0, &[]);
+        let start_height = 10000;
+        let end_height = 30000;
+        let amount = Uint128::new(10_000);
+
+        let msg = ExecuteMsg::RegisterDistribution {
+            start_height,
+            end_height,
+            recipient: RECIPIENT.to_string(),
+            amount,
+            message: None,
+        };
+        let res = entrypoints::execute(deps.as_mut(), mock_env(), info, msg);
+        let distribution_id: u64 = res.unwrap().attributes[1].value.parse().unwrap();
+
+        let msg = ExecuteMsg::Distribute {
+            id: Some(distribution_id),
+        };
+        let mut env = mock_env();
+        env.block.height = 20000;
+
+        let info = mock_info(ADMIN_0, &[]);
+        let res = entrypoints::execute(deps.as_mut(), env.clone(), info, msg).unwrap();
+        assert_eq!(
+            &res.attributes,
+            &vec![
+                attr("action", "distribute"),
+                attr(
+                    "distribution",
+                    format!("{}/{}/{}", distribution_id, RECIPIENT, amount,)
+                )
+            ]
+        );
+        let distributed =
+            amount.multiply_ratio(env.block.height - start_height, end_height - start_height);
+
+        let send_msg = CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr: POLLTERRA_TOKEN.to_string(),
+            funds: vec![],
+            msg: to_binary(&Cw20ExecuteMsg::Transfer {
+                recipient: RECIPIENT.to_string(),
+                amount: distributed,
+            })
+            .unwrap(),
+        });
+        assert_eq!(&res.messages, &vec![SubMsg::new(send_msg)]);
+
+        let msg = QueryMsg::Distributions {};
+        let res: DistributionsResponse =
+            from_binary(&entrypoints::query(deps.as_ref(), env.clone(), msg).unwrap()).unwrap();
+
+        assert_eq!(&start_height, &res.distributions[0].start_height);
+        assert_eq!(&end_height, &res.distributions[0].end_height);
+        assert_eq!(&RECIPIENT, &res.distributions[0].recipient);
+        assert_eq!(&amount, &res.distributions[0].amount);
+        assert_eq!(
+            amount.multiply_ratio(env.block.height - start_height, end_height - start_height)
+                - distributed,
+            res.distributions[0].distributable_amount
+        );
+        assert_eq!(distributed, res.distributions[0].distributed_amount);
+        assert_eq!(
+            amount.multiply_ratio(env.block.height - start_height, end_height - start_height),
+            res.distributions[0].released_amount
+        );
+    }
+
+    #[test]
+    fn proper_transfer() {
+        let mut deps = mock_dependencies(&[]);
+        mock_instantiate(deps.as_mut());
+
+        let initial_amount = Uint128::from(20_000u128);
+        let send_amount = Uint128::from(10_000u128);
+
+        deps.querier.with_token_balances(&[(
+            &POLLTERRA_TOKEN.to_string(),
+            &[(&MOCK_CONTRACT_ADDR.to_string(), &initial_amount)],
+        )]);
+
+        let info = mock_info(ADMIN_0, &[]);
+        let start_height = 10000;
+        let end_height = 30000;
+        let amount = Uint128::new(5_000);
+
+        let msg = ExecuteMsg::RegisterDistribution {
+            start_height,
+            end_height,
+            recipient: RECIPIENT.to_string(),
+            amount,
+            message: None,
+        };
+        let _res = entrypoints::execute(deps.as_mut(), mock_env(), info, msg);
+
+        let info = mock_info(ADMIN_0, &[]);
+        let msg = ExecuteMsg::Transfer {
+            recipient: RECIPIENT_2.to_string(),
+            amount: send_amount,
+        };
+        let res = entrypoints::execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(
+            &res.attributes,
+            &vec![
+                attr("action", "transfer"),
+                attr("requester", ADMIN_0),
+                attr("recipient", RECIPIENT_2),
+                attr("amount", send_amount),
+                attr("remain_amount", initial_amount - amount),
+            ]
+        );
+
+        let msg = CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr: POLLTERRA_TOKEN.to_string(),
+            funds: vec![],
+            msg: to_binary(&Cw20ExecuteMsg::Transfer {
+                recipient: RECIPIENT_2.to_string(),
+                amount: send_amount,
+            })
+            .unwrap(),
+        });
+        assert_eq!(&res.messages, &vec![SubMsg::new(msg)]);
+    }
+
+    #[test]
+    fn transfer_failed_unauthorized() {
+        let mut deps = mock_dependencies(&[]);
+        mock_instantiate(deps.as_mut());
+
+        let initial_amount = Uint128::from(20_000u128);
+        let send_amount = Uint128::from(10_000u128);
+
+        deps.querier.with_token_balances(&[(
+            &POLLTERRA_TOKEN.to_string(),
+            &[(&MOCK_CONTRACT_ADDR.to_string(), &initial_amount)],
+        )]);
+
+        let msg = ExecuteMsg::Transfer {
+            recipient: RECIPIENT_2.to_string(),
+            amount: send_amount,
         };
 
         let info = mock_info(NOT_ADMIN, &[]);
