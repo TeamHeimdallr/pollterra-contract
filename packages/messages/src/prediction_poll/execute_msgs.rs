5cb6c0fda62f3023aedab63ccff84f45aaf179a4
eunho-lee <eunho.ambition@gmail.com>
2022-04-14 18:35:36 +0900

    remove reset_poll

--- packages/messages/src/prediction_poll/execute_msgs.rs
+++ packages/messages/src/prediction_poll/execute_msgs.rs
@@ -10,23 +10,11 @@ pub type InstantiateMsg = PollInstantiateMsg;
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Bet {
-        side: u64,
-    },
-    FinishPoll {
-        winner: u64,
-    },
+    Bet { side: u64 },
+    FinishPoll { winner: u64 },
     RevertPoll {},
     Claim {},
-    ResetPoll {
-        poll_name: String,
-        bet_end_time: u64,
-    },
     ReclaimDeposit {},
-    TransferOwner {
-        new_owner: String,
-    },
-    SetMinimumBet {
-        amount: u128,
-    },
+    TransferOwner { new_owner: String },
+    SetMinimumBet { amount: u128 },
 }
